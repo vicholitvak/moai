f079f04c68966cea9afccefccc3e53df02324932
Vicente Litvak <vicente.litvak@gmail.com>
2025-07-15 04:50:00 +0000

    nothing happends when clicking track your order or continue shopping

--- src/app/order-confirmation/[id]/page.tsx
+++ src/app/order-confirmation/[id]/page.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import { notFound, useParams } from 'next/navigation';
+import { useParams } from 'next/navigation';
 import Image from 'next/image';
 import Link from 'next/link';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
